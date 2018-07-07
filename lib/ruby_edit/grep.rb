31b3670e483185f3b591afcf5fff4687416b9b8e
finn-francis <finn_francis@yahoo.com>
2018-07-07 21:01:42 +0100

    Created class for grepping
    How?
    - Inherit from TTY::Command, to allow bash commands to be performed easily

---
+++ lib/ruby_edit/grep.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'ruby_edit/command'
+
+module RubyEdit
+  class Grep < RubyEdit::Command
+    attr_reader :result
+
+    def initialize(options)
+      @path       = options[:path] || '.'
+      @expression = options[:expression]
+    end
+
+    def search(output: $stdout, errors: $stderr)
+      if empty_expression?
+        output.puts 'No expression given'
+        return false
+      end
+      @result = run "grep -irn #{@path} -e '#{@expression}'" do |out, err|
+        output << out if out
+        errors << err if err
+      end
+    rescue TTY::Command::ExitError => error
+      output.puts error
+    end
+
+    private
+
+    def empty_expression?
+      @expression.nil? || @expression.empty?
+    end
+  end
+end
+
---
+++ spec/unit/grep_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'ruby_edit/grep'
+
+RSpec.describe RubyEdit::Grep do
+  let(:grep) { RubyEdit::Grep.new(options) }
+  let(:options) { { path: '.', expression: 'initialize' } }
+  let(:output) { StringIO.new }
+
+  describe '#search' do
+    it 'should have a getter for result' do
+      expect(grep).to respond_to :result
+    end
+
+    it 'should perform a grep search' do
+      search = grep.search
+      expect(search).to be_a TTY::Command::Result
+      expect(search.out).to_not be_empty
+    end
+
+    context "it doesn't return any matches" do
+      let(:options) { { path: './lib/', expression: 'NON-EXISTING-STRING' } }
+
+      it 'should not error out' do
+        expect { grep.search }.to_not raise_error
+      end
+
+      it 'should print the error' do
+        grep.search(output: output)
+        expect(output.string).to include 'exit status: 1'
+      end
+    end
+
+    context 'no expression is given' do
+      let(:options) { { path: '.', expression: nil } }
+
+      it 'should print a warning' do
+        grep.search(output: output)
+        expect(output.string).to eq "No expression given\n"
+      end
+    end
+  end
+end
+
