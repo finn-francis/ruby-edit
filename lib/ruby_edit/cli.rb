7866dde26a037340d7a70c5b12e264a38ac7f21c
finn-francis <finn_francis@yahoo.com>
2018-07-07 22:35:01 +0100

    Added grep to the cli commands

--- lib/ruby_edit/cli.rb
+++ lib/ruby_edit/cli.rb
@@ -17,6 +17,22 @@ module RubyEdit
       puts "v#{RubyEdit::VERSION}"
     end
     map %w[--version -v] => :version
+
+    desc 'grep', 'search for an expression in your directory'
+    method_option :help, aliases: '-h', type: :boolean, desc: 'Display usage information'
+    method_option :expression, aliases: %w[-e --expression], type: :string,
+                               desc: 'the grep expression'
+    method_option :path, aliases: %w[-p --path], type: :string, desc: 'the path you want to search'
+    def grep(*)
+      if options[:help]
+        invoke :help, ['grep']
+      else
+        require_relative 'commands/grep'
+        RubyEdit::Commands::Grep.new(options).execute
+      end
+    end
+
+    map %w[--grep -g] => :grep
   end
 end
 
---
+++ spec/integration/grep_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe '`ruby_edit grep` command', type: :cli do
+  let(:output) { `ruby-edit grep -e initialize` }
+
+  it 'should run the grep command' do
+    expect(output).to include('initialize', 'OK')
+  end
+
+  describe 'help' do
+    let(:output) { `ruby-edit help grep` }
+    let(:usage) do
+      "Usage:\n  ruby-edit grep"
+    end
+    let(:help) { '-h, [--help], [--no-help]' }
+    let(:expression) { '-e, --expression, [--expression=EXPRESSION]' }
+    let(:path) { '-p, --path, [--path=PATH]' }
+
+    it 'should execute the `ruby-edit help grep` command successfully' do
+      expect(output).to include(usage, help, expression, path)
+    end
+  end
+end
+
