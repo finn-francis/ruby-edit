3c3b8e4490b4b000309994397b6e84023bc700ff
finn-francis <finn_francis@yahoo.com>
2018-07-08 19:02:34 +0100

    Set @config in the initializer for RubyEdit::Editor
    Why?
    - I was getting this error: "undefined method `read' for TTY::File:Module"
      Still not sure what was causing it, but setting @config in the initializer sorted out the issue

--- lib/ruby_edit/editor.rb
+++ lib/ruby_edit/editor.rb
@@ -4,8 +4,12 @@ require 'ruby_edit/command'
 
 module RubyEdit
   class Editor < RubyEdit::Command
+    def initialize
+      @config = RubyEdit.config
+    end
+
     def edit_sourcefile
-      editor.open(RubyEdit::SOURCE_FILE_LOCATION, command: RubyEdit.config.editor)
+      editor.open RubyEdit::SOURCE_FILE_LOCATION, command: @config.editor
     end
   end
 end
