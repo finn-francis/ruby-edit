4ec0650b1c5b744ee74888797b2ce476ade2a6e6
finn-francis <finn_francis@yahoo.com>
2018-07-10 00:41:25 +0100

    Installed and setup simplecov
    Why?
    - Monitor test coverage

---
+++ .simplecov
@@ -0,0 +1,5 @@
+SimpleCov.start do
+  add_filter '/spec/'
+  add_group 'Commands', 'lib/ruby_edit/commands'
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'simplecov'
 require 'ruby_edit'
 
 RSpec.configure do |config|
