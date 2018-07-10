b26dbaa7e8db11e5f30c1f116a9d51a895db3353
finn-francis <finn_francis@yahoo.com>
2018-07-10 01:05:17 +0100

    Added a spec to ensure RubyEdit::Writer#split_file_and_line doesn't error out
    How?
    - Write a sourcefile with a broken line to ensure it succesfully rescues the error and continues with the writing.
    
    Why?
    - To make sure that all lines have been covered in the RubyEdit::Writer spec.

--- spec/unit/writer_spec.rb
+++ spec/unit/writer_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe RubyEdit::Writer do
   let(:generator) { RubyEdit::Command.new.generator }
   before do
     stub_const 'RubyEdit::SOURCE_FILE_LOCATION', 'spec/support/sourcefile'
+  end
+  after do
     generator.copy_directory('spec/support/test_directory', 'spec/support/temp_directory',
                              force: true)
   end
@@ -25,6 +27,16 @@ RSpec.describe RubyEdit::Writer do
       expect(File.read(file2)).to include 'CHANGED_TEXT'
       expect(File.read(file1)).to_not include 'TEXT_TO_CHANGE'
       expect(File.read(file2)).to_not include 'TEXT_TO_CHANGE'
+    end
+
+    context 'there is a NoMethodError in #split_file_and_line' do
+      before do
+        stub_const 'RubyEdit::SOURCE_FILE_LOCATION', 'spec/support/broken_sourcefile'
+      end
+
+      it 'should not interrupt the writing' do
+        expect { writer.write }.to_not raise_error
+      end
     end
   end
 end
