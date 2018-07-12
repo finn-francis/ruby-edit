177734590f8de4cefdd9b2a66cdb33a82f40b39c
finn-francis <finn_francis@yahoo.com>
2018-07-13 00:52:26 +0100

    Added **options to RubyEdit::SourceFile #populate and #delete
    Why?
    - Allow extra options to be passed through in the future e.g. user configs

--- lib/ruby_edit/source_file.rb
+++ lib/ruby_edit/source_file.rb
@@ -4,12 +4,12 @@ require 'ruby_edit/command'
 
 module RubyEdit
   class SourceFile < RubyEdit::Command
-    def populate(content)
-      generator.create_file(RubyEdit::SOURCE_FILE_LOCATION, content, force: true)
+    def populate(content, **options)
+      generator.create_file(RubyEdit::SOURCE_FILE_LOCATION, content, force: true, **options)
     end
 
-    def delete
-      generator.remove_file(RubyEdit::SOURCE_FILE_LOCATION)
+    def delete(**options)
+      generator.remove_file(RubyEdit::SOURCE_FILE_LOCATION, **options)
     end
   end
 end
